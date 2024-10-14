1eea816468f4c9530f6f743572de0627907a55d1
MahmoudMohamed14 <mm3188@fayoum.edu.eg>
2024-10-14 21:44:35 +0300

    search

--- lib/features/payment page/controller/order_data_handler.dart
+++ lib/features/payment page/controller/order_data_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import '../../../Core/Api/generic_request.dart';
 import '../../../Core/Api/http_request.dart';
 import '../../../Core/Error/exceptions.dart';
@@ -47,6 +49,7 @@ class PostOrderDataHandler {
       });
       bool response = await GenericRequest<bool>(
         method: HttpRequestHandler.postJson(
+
             url: ApiEndPoint.orders,
             bodyJson: {
               "couponId": coupon,
@@ -55,6 +58,50 @@ class PostOrderDataHandler {
             }),
         fromMap: (data) {
           return data["statusCode"] == 200||data["statusCode"] == 201;
+        },
+      ).getResponse(printBody: false);
+      return Either.right(response);
+    } on ServerException catch (failure) {
+      return Either.left(
+        ServerFailure(failure.errorMessageModel),
+      );
+    }
+  }
+  static Future<Either<Failure, Map<String, dynamic>>> paymentMethod() async {
+    final data = {
+    "amount": 600,
+    "currency": "SAR",
+    "description": "Payment for order #123456789",
+    "callback_url": "https://example.com/thankyou",
+    "source": {
+    "type": "creditcard",
+    "name": "mahmoud mohamed",
+    "number": "4201320111111010",
+    "cvc": "123",
+    "month": "12",
+    "year": "26"
+    }};
+
+
+    const apiKey = 'pk_test_Z9MJdDVC96N12UzYPUgdkLREmQJHCmHdjZzY5EZm';
+    final encodedApiKey = base64Encode(utf8.encode(apiKey));
+
+    // Request headers
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic $encodedApiKey',
+    };
+    try {
+
+      Map<String, dynamic> response = await GenericRequest<Map<String, dynamic>>(
+        method: HttpRequestHandler.postJson(
+          headers:headers ,
+
+            url: 'https://api.moyasar.com/v1/payments',
+            bodyJson: data),
+        fromMap: (data) {
+          print("data is $data");
+          return data;
         },
       ).getResponse(printBody: false);
       return Either.right(response);
