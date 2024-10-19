a8ec3a0c28e6b06325d1159fa40af064b7ccede6
MahmoudMohamed14 <mm3188@fayoum.edu.eg>
2024-10-19 23:28:35 +0300

    coupon

--- lib/features/payment page/widget/order_money.dart
+++ lib/features/payment page/widget/order_money.dart
@@ -6,7 +6,7 @@ import '../../../common/constant/constant values.dart';
 import '../../../common/translate/strings.dart';
 import '../controller/order_cubit.dart';
 
-Widget orderMoney(text,price,oldPrice,check,context)=>Padding(
+Widget orderMoney(text,price,oldPrice,check,context,{Color ?colorPrice})=>Padding(
   padding: const EdgeInsets.only(left: 8.0,right: 8),
   child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [
     Text('$text',style:const TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
@@ -15,9 +15,9 @@ Widget orderMoney(text,price,oldPrice,check,context)=>Padding(
         Text('$oldPrice',style: TextStyle(fontSize: 16 ,decoration: TextDecoration.lineThrough,color: isDark??false?Colors.white:borderColor),),
       if(OrderCubit.get(context).couponCode!=null)
         const SizedBox(width: 10,),
-      Text('$price',style: const TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
+      Text('$price',style:  TextStyle(fontWeight: FontWeight.w500,fontSize: 18,color: colorPrice),),
       const SizedBox(width: 3,),
-      Text(Strings.sr.tr(context),style:const TextStyle(fontWeight: FontWeight.w500,fontSize: 15),)
+      Text(Strings.sr.tr(context),style: TextStyle(fontWeight: FontWeight.w500,fontSize: 15,color: colorPrice),)
     ],)
   ],),
 );
